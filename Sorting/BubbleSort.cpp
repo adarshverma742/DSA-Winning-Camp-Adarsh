5bb33e52925f142f47e5a7240cfd2fe07cff82a5
adarshverma742 <117636043+adarshverma742@users.noreply.github.com>
2023-01-11 12:42:32 +0530

    Create BubbleSort.cpp

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,23 @@
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        int temp;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+};
